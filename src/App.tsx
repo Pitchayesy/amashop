d247113d27706ffc5015dda5c98b5b81f1da2b05
Pitchayesy <152055854+Pitchayesy@users.noreply.github.com>
2025-12-31 11:40:44 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/amashop">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/categories" element={<Categories />} />
